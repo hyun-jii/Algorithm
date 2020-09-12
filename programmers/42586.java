517e915ce29b56e69e2c533d49aa960e63deecc1
LEEHYUNJEE <54610824+hyun-jii@users.noreply.github.com>
2020-09-12 17:36:14 +0900

    #기능개발

---
+++ programmers/42586.java
@@ -0,0 +1,40 @@
+// 평균 속도 0.2ms 평균 메모리 68MB  
+import java.util.*;
+
+class Solution {
+    public Queue<Integer> solution(int[] progresses, int[] speeds) {
+        Queue<Integer> leftDays = new LinkedList<>();
+        Queue<Integer> result = new LinkedList<>();
+    
+        for(int i=0; i<progresses.length; i++) {
+            int left = 100 - progresses[i];   
+            int leftDay = left / speeds[i];
+            
+            if(left%speeds[i] != 0) {
+                leftDay++;
+            }
+            leftDays.add(leftDay);
+            leftDay = 0;
+        }
+
+        int count = 1;
+        int deploy = leftDays.remove();
+        
+        while(!leftDays.isEmpty()) {
+            if(deploy >= leftDays.element()) {
+                count++;
+                leftDays.remove();
+            }
+            else if(deploy < leftDays.element()) {
+                result.add(count);
+                count = 1;
+                deploy = leftDays.remove();
+            }
+            
+            if(leftDays.isEmpty()) {
+                result.add(count);
+            }           
+        }     
+        return result;
+    }
+}
