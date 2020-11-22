fe7e49291a50349ce1bbe97ec932b32ad724f4a7
LEEHYUNJEE <54610824+hyun-jii@users.noreply.github.com>
2020-11-22 18:47:49 +0900

    #전화번호 목록

--- programmers/42577.java
+++ programmers/42577.java
@@ -1,30 +1,30 @@
 /*
 정확성  테스트
-테스트 1 〉	통과 (0.03ms, 52.2MB)
-테스트 2 〉	통과 (0.02ms, 52.3MB)
-테스트 3 〉	통과 (0.02ms, 52.4MB)
-테스트 4 〉	통과 (0.02ms, 52.8MB)
-테스트 5 〉	통과 (0.04ms, 52.6MB)
-테스트 6 〉	통과 (0.03ms, 52.5MB)
-테스트 7 〉	통과 (0.02ms, 52.1MB)
-테스트 8 〉	통과 (0.02ms, 51.9MB)
-테스트 9 〉	통과 (0.03ms, 52.9MB)
-테스트 10 〉	통과 (0.02ms, 52.2MB)
-테스트 11 〉	통과 (0.02ms, 52.6MB)
+테스트 1 〉	통과 (0.03ms, 51.8MB)
+테스트 2 〉	통과 (0.02ms, 52.9MB)
+테스트 3 〉	통과 (0.02ms, 52.2MB)
+테스트 4 〉	통과 (0.03ms, 52.5MB)
+테스트 5 〉	통과 (0.02ms, 52.9MB)
+테스트 6 〉	통과 (0.03ms, 51.8MB)
+테스트 7 〉	통과 (0.02ms, 53.7MB)
+테스트 8 〉	통과 (0.02ms, 53.1MB)
+테스트 9 〉	통과 (0.02ms, 52.1MB)
+테스트 10 〉	통과 (0.02ms, 52MB)
+테스트 11 〉	통과 (0.02ms, 52.2MB)
 효율성  테스트
-테스트 1 〉	통과 (1.88ms, 56.1MB)
-테스트 2 〉	통과 (1.88ms, 57MB)
+테스트 1 〉	통과 (1.50ms, 56.8MB)
+테스트 2 〉	통과 (1.38ms, 57.5MB)
 */
 
 import java.util.*;
 
 class Solution {
-    public boolean solution(String[] phone_book) {        
+    public boolean solution(String[] phone_book) {
         for(int i=0; i<phone_book.length; i++) {
+            String number = phone_book[i];
             for(int j=0; j<phone_book.length; j++) {
-                if(phone_book[j].length() > phone_book[i].length()) {
-                    String headNumber = phone_book[j].substring(0, phone_book[i].length());
-                    if(headNumber.equals(phone_book[i])) {
+                if(phone_book[j].length() > number.length()) {
+                    if(phone_book[j].startsWith(number)) {
                         return false;
                     }
                 }
